39bf5dc6a93f46939f847d260e269fb6cc729bd4
SuzLQ <ysus12061986@gmail.com>
2023-09-07 21:59:49 -0400

    Revert "update HelloTekWorld.java"
    This reverts commit 5fe32d1820e7ecc6db58d139146d4061ad65664b.

---
+++ HelloTekWorld.java
@@ -0,0 +1,10 @@
+public class HelloTekWorld{
+    
+     public static void main(String[] args)
+     { 
+        System.out.println("Hello world!");
+        System.out.println("Program modified, but able to revert to original version using GitHub Desktop." );
+  
+    }
+ }
+
